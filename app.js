653ab767aeaea0325ffbc52c9ad2615afcf8ed4b
Harish Narain <harishnarain@gmail.com>
2020-10-21 22:50:12 +1100

    Added Team class, welcomeUser and getTeamInput methods.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
@@ -9,10 +5,23 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const welcomeUser = require("./lib/welcomeUser");
+const { inherits } = require("util");
+const getTeamInput = require("./lib/getTeamInput");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const init = async () => {
+  // welcome message
+  welcomeUser();
+
+  // get employees and add to team object
+  const team = await getTeamInput();
+  console.log(team.getEmployees());
+
+  // render team
+  // output team.html
+};
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +42,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+init();
